df694f364e5a6ec14b56f79e285624a5cddeed36
StevenChengYan <73865522+StevenChengYan@users.noreply.github.com>
2020-11-05 13:59:32 +0800

    Update 13_Ringer.v

--- Verilog Language/Circuits/Combinational Logic/Basic Gates/13_Ringer.v
+++ Verilog Language/Circuits/Combinational Logic/Basic Gates/13_Ringer.v
@@ -4,6 +4,11 @@ module top_module (
     output ringer,       // Make sound
     output motor         // Vibrate
 );
+    assign motor = vibrate_mode & ring;
+    assign ringer = (~vibrate_mode) & ring;
+    
+    /*
     assign motor = vibrate_mode ? ring : 0;
     assign ringer = vibrate_mode ? 0 : ring;
+    */
 endmodule
