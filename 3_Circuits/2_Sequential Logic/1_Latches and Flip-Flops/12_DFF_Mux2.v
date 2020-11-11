f733d2d5254755671cc267a8cf38692545ff8558
StevenChengYan <73865522+StevenChengYan@users.noreply.github.com>
2020-11-11 12:01:59 +0800

    Create 12_DFF_Mux2.v

---
+++ 3_Circuits/2_Sequential Logic/1_Latches and Flip-Flops/12_DFF_Mux2.v
@@ -0,0 +1,22 @@
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output Q
+);
+	wire w1, w_din;
+    always@(*) begin
+        case(E)
+            0: w1 = Q;
+            1: w1 = w;
+        endcase
+        case(L)
+            0: w_din = w1;
+            1: w_din = R;
+        endcase
+    end
+    
+    always@(posedge clk) begin
+        Q <= w_din;
+    end
+    
+endmodule
