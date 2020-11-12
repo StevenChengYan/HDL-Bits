5cf9d0c0a31c03f63be646c6fa0b5d7b67529fe9
StevenChengYan <73865522+StevenChengYan@users.noreply.github.com>
2020-11-12 15:59:07 +0800

    Create 3_And_gate_tb.v

---
+++ 4_Verification/3_Writing_Testbench/3_And_gate_tb.v
@@ -0,0 +1,16 @@
+module top_module();
+	
+    reg [1:0] in;
+    wire out;
+    
+    initial begin
+       #0  in = 2'b00;
+       #10 in = 2'b01;
+       #10 in = 2'b10;
+       #10 in = 2'b11;
+    end
+    
+    andgate dut_gate(.in(in), .out(out));
+
+
+endmodule
