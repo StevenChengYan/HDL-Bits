90475b8fc2d32aef05592b0f77c1df94a6337837
StevenChengYan <73865522+StevenChengYan@users.noreply.github.com>
2020-11-13 15:00:13 +0800

    Create 1_SimpleFSM1.v

---
+++ 3_Circuits/2_Sequential Logic/5_Finite_State_Machine/1_SimpleFSM1.v
@@ -0,0 +1,32 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to state B
+    input in,
+    output out);//  
+
+    parameter A=0, B=1; 
+    reg state, next_state;
+
+    //Next state logic
+    always @(*) begin    // This is a combinational always block
+        // State transition logic
+        case (state)
+            B: if(in==0) next_state <= A;
+               else      next_state <= B;
+            A: if(in==0) next_state <= B;
+               else      next_state <= A;
+            default:     next_state <= B;
+        endcase
+    end
+
+    //State register
+    always @(posedge clk, posedge areset) begin    // This is a sequential always block
+        // State flip-flops with asynchronous reset
+        if(areset) state <= B;
+		else state <= next_state;
+    end
+
+    // Output logic
+    assign out = (state == B) ? 1:0;
+
+endmodule
